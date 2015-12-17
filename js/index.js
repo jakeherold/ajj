40aa22841d31241c140ca24ca70a827b05cd3068
Jacob Porter Herold <dcejake@gmail.com>
2015-12-17 08:33:11 -0800

    commmiting final changes. Pulls gas data like a dream

--- js/index.js
+++ js/index.js
@@ -1,22 +1,26 @@
 $(function(){
 
+  var gas = {};
+  // var gas.rawData;
+  $.ajax({
+    type:'GET',
+    url: 'http://www.fueleconomy.gov/ws/rest/fuelprices',
+    success: function(data){
+      console.log(data);
+      gas.rawData = xmlToJson(data);
+      console.log(data);
+    gas.regular = gas.rawData.fuelPrices.regular["#text"];
+    console.log(gas.regular);
+    gas.midgrade = gas.rawData.fuelPrices.midgrade["#text"];
+    console.log(gas.midgrade);
+    gas.premium = gas.rawData.fuelPrices.premium["#text"];
+    console.log(gas.premium);
 
-// $.ajax({
-//   type:'GET',
-//   url: 'http://www.fueleconomy.gov/ws/rest/fuelprices',
-//   success: function(data){
-//     console.log(data);
+    }
+  });
 
 
-//   }
-// });
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://www.fueleconomy.gov/ws/rest/fuelprices', true);
-xhr.send();
 
-xhr.onload = function(data){
-  console.log(data);
-}
-console.log("what the fuck is goin on here?");
+
 
 }); //ends IIFE
