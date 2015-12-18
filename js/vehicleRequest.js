167b624200cfe539678ce9d9395f5e522eeac0b1
andalex <and4alex@gmail.com>
2015-12-18 14:59:32 -0800

    added filter option for vehicle version selection. fixed css checkboxes to display checks.

--- js/vehicleRequest.js
+++ js/vehicleRequest.js
@@ -1,13 +1,14 @@
-var metaMpgData = {};
+var metaMpgData    = {};
 var vehicleRequest = {};
-var $vehicleDefer = $.Deferred();
+var $vehicleDefer  = $.Deferred();
 vehicleRequest.index = function() {
-    var $carYear = $('.carYear');
-    var $carMake = $('.carMake');
-    var $carModel = $('.carModel');
-    var $avgMpg = $('.avgMpg');
-    var $minMpg = $('.minMpg');
-    var $maxMpg = $('.maxMpg');
+    var $carYear      = $('.carYear');
+    var $carMake      = $('.carMake');
+    var $carModel     = $('.carModel');
+    var $carVersion   = $('.carVersion');
+    var $avgMpg       = $('.avgMpg');
+    var $minMpg       = $('.minMpg');
+    var $maxMpg       = $('.maxMpg');
     var $errorVehicle = $('.errorVehicle');
 
     var date = new Date();
@@ -58,54 +59,67 @@ vehicleRequest.index = function() {
     });
 
     $carModel.on('change', function() {
-        userCarId();
+        userCarVersion();
     });
-
-    function userCarId() {
-        userYear = $(".carYear option:selected").text();
-        userMake = $(".carMake option:selected").text();
-        userModel = $(".carModel option:selected").text();
+    function userCarVersion() {
+        userYear    = $(".carYear option:selected").text();
+        userMake    = $(".carMake option:selected").text();
+        userModel   = $(".carModel option:selected").text();
         ajaxRequest = $.ajax({
             type: "GET",
-            url: '//www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=' + userYear + '&make=' + userMake + '&model=' + userModel,
+            url: 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=' + userYear + '&make=' + userMake + '&model=' + userModel,
             dataType: "xml",
         });
         ajaxRequest.done(function(xml) {
-            var vehicleID = $(xml).find("value").first().text();
-            console.log(vehicleID);
-            ajaxRequest = $.ajax({
-                type: "GET",
-                url: '//www.fueleconomy.gov/ws/rest/ympg/shared/ympgVehicle/' + vehicleID,
-                dataType: "xml",
-                statusCode: {
-                    404: function() {
-                      //resets and error message in drop down section
-                      $errorVehicle.append('Sorry, we could not find that vehicle.')
-                      $carYear.val(0);
-                      $carMake.val(0);
-                      $carModel.val(0);
-                    }
-                }
+            $(xml).find("text").each(function() {
+                $carVersion.append('<option>' + $(this).text() + '</option>');
             });
-            ajaxRequest.done(function(xml) {
-                $(xml).find("avgMpg").each(function() {
-                    metaMpgData.avgmpg = $(this).text();
-                    $avgMpg.append($(this).text());
-                });
-                $(xml).find("maxMpg").each(function() {
-                    metaMpgData.maxmpg = $(this).text();
-                    $maxMpg.append($(this).text());
-                });
-                $(xml).find("minMpg").each(function() {
-                    metaMpgData.minmpg = $(this).text();
-                    $minMpg.append($(this).text());
-                    console.log(metaMpgData);
-                });
-                $vehicleDefer.resolve();
-                console.log("vehicle defer resolved");
+            var vehicleID = $(xml).find("text:contains('" + $carVersion.val() + "')").next("value").text();
+            console.log(vehicleID);
 
-            })
-        });
-    } //userCarId
+            $carVersion.on('change', function() {
+                userCarId();
+                console.log(vehicleID);
+            });
+
+            function userCarId() {
+                ajaxRequest = $.ajax({
+                    type: "GET",
+                    url: 'http://www.fueleconomy.gov/ws/rest/ympg/shared/ympgVehicle/' + vehicleID,
+                    dataType: "xml",
+                    statusCode: {
+                        404: function() {
+                            //resets and error message in drop down section
+                            $errorVehicle.append('Sorry, we could not find that vehicle.')
+                            $carYear.val(0);
+                            $carMake.val(0);
+                            $carModel.val(0);
+                            $carVersion.val(0);
+                        }
+                    }
+                });
+                console.log(xml);
+                ajaxRequest.done(function(xml) {
+                    $(xml).find("avgMpg").each(function() {
+                        metaMpgData.avgmpg = Math.round(parseInt($(this).text()));
+                        console.log(metaMpgData.avgmpg);
+                        $avgMpg.append($(this).text());
+                    });
+                    $(xml).find("maxMpg").each(function() {
+                        metaMpgData.maxmpg = $(this).text();
+                        $maxMpg.append($(this).text());
+                    });
+                    $(xml).find("minMpg").each(function() {
+                        metaMpgData.minmpg = $(this).text();
+                        $minMpg.append($(this).text());
+                        console.log(metaMpgData);
+                    });
+                    $vehicleDefer.resolve();
+                    console.log("vehicle defer resolved");
+
+                })
+            } //userCarId
+        }); //ajax done #1
+    } //userCarVersion
 }; //vehicleRequest closed
 vehicleRequest.index();
