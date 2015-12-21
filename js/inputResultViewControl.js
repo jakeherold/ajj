17613f7f3f7827532d71988d4f2b50932e74dfcd
Jacob Porter Herold <dcejake@gmail.com>
2015-12-21 14:12:55 -0800

    changed inputresults

--- js/inputResultViewControl.js
+++ js/inputResultViewControl.js
@@ -3,15 +3,15 @@ $(function(){
   localStorageData = localStorage.getItem('avgMpg');
   if (localStorageData){
     console.log("Local Storage Data Exists");
-    user.avgMpg      =  localStorage.getItem('avgMpg');
-    user.maxMpg      = localStorage.getItem('maxMpg');
-    user.minMpg      = localStorage.getItem('minMpg');
-    user.gasQuantityAvg = localStorage.getItem('gasQuantityAvg');;
-    user.gasQuantityMax = localStorage.getItem('gasQuantityMax');
-    user.gasQuantityMin = localStorage.getItem('gasQuantityMin');
-    user.costReg     =  localStorage.getItem('costReg');
-    user.costMid     =  localStorage.getItem('costMid');
-    user.costPrem    =  localStorage.getItem('costPrem');
+    user.avgMpg         =  localStorage.getItem('avgMpg');
+    user.maxMpg         =  localStorage.getItem('maxMpg');
+    user.minMpg         =  localStorage.getItem('minMpg');
+    user.gasQuantityAvg =  localStorage.getItem('gasQuantityAvg');;
+    user.gasQuantityMax =  localStorage.getItem('gasQuantityMax');
+    user.gasQuantityMin =  localStorage.getItem('gasQuantityMin');
+    user.costReg        =  localStorage.getItem('costReg');
+    user.costMid        =  localStorage.getItem('costMid');
+    user.costPrem       =  localStorage.getItem('costPrem');
     console.log(user);
     // $('#userInput').hide();
     // $('#pageResults').show();
