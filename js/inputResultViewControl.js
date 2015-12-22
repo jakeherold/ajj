4e0f5617d3baf6fce3df0ea9691e4c0bf5eb16ac
Jacob Porter Herold <dcejake@gmail.com>
2015-12-22 08:21:46 -0800

    changed long line of code to function

--- js/inputResultViewControl.js
+++ js/inputResultViewControl.js
@@ -9,23 +9,25 @@ $(function(){
   if (localStorageData){
     $('#prevLoad').show();
     console.log("Local Storage Data Exists");
-    user.avgMpg         =  localStorage.getItem('avgMpg');
-    user.maxMpg         =  localStorage.getItem('maxMpg');
-    user.minMpg         =  localStorage.getItem('minMpg');
-    user.gasQuantityAvg =  localStorage.getItem('gasQuantityAvg');;
-    user.gasQuantityMax =  localStorage.getItem('gasQuantityMax');
-    user.gasQuantityMin =  localStorage.getItem('gasQuantityMin');
-    user.costReg        =  localStorage.getItem('costReg');
-    user.costMid        =  localStorage.getItem('costMid');
-    user.costPrem       =  localStorage.getItem('costPrem');
-    console.log(user);
+    buildUserObjectFromStorage();
   }
   else {
     console.log("Local data not set. Need user input");
     $('#prevLoad').hide();
   }
 });
-
+function buildUserObjectFromStorage () {
+  user.avgMpg         =  localStorage.getItem('avgMpg');
+  user.maxMpg         =  localStorage.getItem('maxMpg');
+  user.minMpg         =  localStorage.getItem('minMpg');
+  user.gasQuantityAvg =  localStorage.getItem('gasQuantityAvg');;
+  user.gasQuantityMax =  localStorage.getItem('gasQuantityMax');
+  user.gasQuantityMin =  localStorage.getItem('gasQuantityMin');
+  user.costReg        =  localStorage.getItem('costReg');
+  user.costMid        =  localStorage.getItem('costMid');
+  user.costPrem       =  localStorage.getItem('costPrem');
+  console.log(user);
+}
 //routing
   page.base('/');
   page('home', myHome);
